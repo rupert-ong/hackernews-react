3621a751222d9c885baf124f25ea045400c01653
rupert-ong <rupert.ong@gmail.com>
2018-03-25 15:18:55 -0400

    Add Hot Module Replacement

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const helloWorld = `Welcome to The Road to React`;
+    const user = {
+      firstName: 'Johnny',
+      lastName: 'Cage'
+    };
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <h2>{helloWorld}, {user.firstName} {user.lastName}</h2>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
