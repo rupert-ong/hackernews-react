edd50f8cb8323b650ac56a428a5159907148b98b
rupert-ong <rupert.ong@gmail.com>
2018-03-26 22:00:50 -0400

    Add state management to component

--- src/App.js
+++ src/App.js
@@ -2,33 +2,42 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'http://facebook.github.io/react',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectId: 0
+  },
+  {
+    title: 'Redux',
+    url: 'http://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectId: 1
+  }
+];
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list,
+    }
+  }
   render() {
-    const list = [
-      {
-        title: 'React',
-        url: 'http://facebook.github.io/react',
-        author: 'Jordan Walke',
-        num_comments: 3, 
-        points: 4,
-        objectId: 0
-      },
-      {
-        title: 'Redux',
-        url: 'http://github.com/reactjs/redux',
-        author: 'Dan Abramov, Andrew Clark',
-        num_comments: 2, 
-        points: 5,
-        objectId: 1
-      }
-    ];
+    const { list } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        {list.map(item => 
+        {list.map(item =>
           <div key={item.objectId}>
             <span><a href={item.url}>{item.title}</a></span>
             <span>{item.author}</span>
