3b54e92daa6b5223790dbe5a669c3dece4960096
rupert-ong <rupert.ong@gmail.com>
2018-03-27 21:48:55 -0400

    Style index and components

--- src/App.js
+++ src/App.js
@@ -46,22 +46,20 @@ class App extends Component {
   render() {
     const { list, searchTerm } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Search
-          value={searchTerm}
-          onChange={this.onSearchChange}
-        >
-          Search
-        </Search>
-        <Table
-          list={list}
-          pattern={searchTerm}
-          onDismiss={this.onDismiss}
-        />
+      <div className="page">
+        <div className="interactions">
+          <Search
+            value={searchTerm}
+            onChange={this.onSearchChange}
+          >
+            Search
+          </Search>
+          <Table
+            list={list}
+            pattern={searchTerm}
+            onDismiss={this.onDismiss}
+          />
+        </div>
       </div>
     );
   }
@@ -78,16 +76,17 @@ const Search = ({ searchTerm, onChange, children }) =>
   </form>
 
 const Table = ({ list, onDismiss, pattern }) =>
-  <div>
+  <div className="table">
     {list.filter(isSearched(pattern)).map(item =>
-      <div key={item.objectId}>
-        <span><a href={item.url}>{item.title}</a></span>{' '}
-        <span>{item.author}</span>{' '}
-        <span>{item.num_comments}</span>{' '}
-        <span>{item.points}</span>{' '}
-        <span>
+      <div key={item.objectId} className="table-row">
+        <span style={{ width: '40%' }}><a href={item.url}>{item.title}</a></span>
+        <span style={{ width: '30%' }}>{item.author}</span>
+        <span style={{ width: '10%' }}>{item.num_comments}</span>
+        <span style={{ width: '10%' }}>{item.points}</span>
+        <span style={{ width: '10%' }}>
           <Button
             onClick={() => onDismiss(item.objectId)}
+            className="button-inline"
           >
             Dismiss
           </Button>
